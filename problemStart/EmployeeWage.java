78844b8c09eecd35c143e1b7342cf69d6722c8c3
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-29 02:41:29 +0530

    ADDED:Displayed Employee Wage of day

--- problemStart/EmployeeWage.java
+++ problemStart/EmployeeWage.java
@@ -1,14 +1,18 @@
-//START JAVA EMPWAGECOMPUTATION PROGRAM//
 public class  EmployeeWage{
 	public static void main(String[] args){
 		//CONSTANTS//
 		int IS_FULL_TIME=1;
+		int EMP_RATE_PER_HOUR=20;
+		//WE ARE TAKING VARIABLE
+		int empHrs=0;
+		int empWage=0;
 		//COMPUTATION//
 		double empCheck = Math.floor(Math.random()*10)%2;
 		if (empCheck == IS_FULL_TIME)
-			System.out.println( "Employee is present" );
+			empHrs=8;
 		else
-			System.out.println( "Employee is a not Abscent" );
+			empHrs=0;
+			empWage=empHrs*EMP_RATE_PER_HOUR;
+			System.out.println( "Emp Wage:" +empWage);
 	}
 }
-

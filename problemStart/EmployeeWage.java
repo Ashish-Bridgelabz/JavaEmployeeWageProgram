bd44e8bbfccbadc9a1c80f351be79fef6375bd12
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-31 09:30:25 +0530

    ADDED:Finded total wage for each company using instance variable

--- problemStart/EmployeeWage.java
+++ problemStart/EmployeeWage.java
@@ -1,24 +1,32 @@
 public class  EmployeeWage{
-	public static final  int IS_PART_TIME=1;
-	public static final int IS_FULL_TIME=2;
-	public static int EMP_RATE_PER_HOUR;
-	public static int NUM_OF_WORKING_DAY;
-	public static int MAX_HRS_IN_MONTH;
-
+	private static final  int IS_PART_TIME=1;
+	private static final int IS_FULL_TIME=2;
+	private final int EMP_RATE_PER_HOUR;
+	private final int NUM_OF_WORKING_DAY;
+	private final int MAX_HRS_IN_MONTH;
+	int totalEmpWage;
 	//WE ARE TAKING CONSTRUCTOR PARAMETER
 	public EmployeeWage(int EMP_RATE_PER_HOUR,int NUM_OF_WORKING_DAY,int MAX_HRS_IN_MONTH){
 		this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
 		this.NUM_OF_WORKING_DAY = NUM_OF_WORKING_DAY;
 		this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
 	}
+ 	public static void main (String args[]){
+         EmployeeWage company1 = new EmployeeWage(20,20,100);
+         employeeCompany(company1);
+			EmployeeWage company2 = new EmployeeWage(20,20,50);
+         employeeCompany(company2);
+	}
+	public void setEmpWage(int totalEmpWage){
+ 	this.totalEmpWage = totalEmpWage;
+	}
 
-	//	WE ARE USING METHOD
-	public static int	employeeCompany(){ 
+	private static void employeeCompany(EmployeeWage employeeWage){ 
 		//WE ARE TAKING LOCAL VARIABLE
-		int empHrs=0,totalEmpHrs=0,totalWorkingDays=0;
-			while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAY){
-				totalWorkingDays++;
-				int empCheck = (int) (Math.random()*10)%3;
+		int empHrs=0,totalEmpHrs=0,totalWorkingDays=0,empWage=0,totalEmpWage=0;
+			while (totalEmpHrs <= employeeWage.MAX_HRS_IN_MONTH && totalWorkingDays < employeeWage.NUM_OF_WORKING_DAY){
+					totalWorkingDays++;
+					int empCheck = (int) (Math.random()*10)%3;
 				switch (empCheck){
 					case IS_PART_TIME:
 							empHrs=4;
@@ -30,27 +38,14 @@ public class  EmployeeWage{
 							empHrs=0;
 				}
 				totalEmpHrs += empHrs;
+				empWage = empHrs * employeeWage.EMP_RATE_PER_HOUR;
+				totalEmpWage += empWage;
 				System.out.println("Day:" +totalWorkingDays + "Emp Hr:"+empHrs);
 			}
-			return totalEmpHrs;
+			employeeWage.setEmpWage(totalEmpWage);
+			System.out.println("Total empWage for company2:" +totalEmpWage);
 	}
 
-		public int findTotalEmpWage( int totalEmpHrs ){
-				return totalEmpHrs * EMP_RATE_PER_HOUR;
-		}
-
-		public static void main (String args[]){
-			EmployeeWage company1 = new EmployeeWage(20, 15, 50);
-			int totalEmployeeHrs = company1.employeeCompany();
-			int totalEmployeeWages	=company1.findTotalEmpWage(totalEmployeeHrs);
-			System.out.println("Total employee wages of company1:" +totalEmployeeWages);
-
-			EmployeeWage company2 = new EmployeeWage(30, 15, 60);
-			int totalEmployeeHrs1 = company2.employeeCompany();
-			int totalEmployeeWages1 = company2.findTotalEmpWage(totalEmployeeHrs1);
-			System.out.println("Total employee Wage of company2:" +totalEmployeeWages);
-
-		}
 }
 
 

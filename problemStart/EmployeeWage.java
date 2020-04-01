ba721f6caf7bb45796c06e0c6d6dd5a1a4b3e019
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-01 19:05:54 +0530

    ADDED:Using interface for multiple companies of EmpWage

--- problemStart/EmployeeWage.java
+++ problemStart/EmployeeWage.java
@@ -1,76 +1,81 @@
- public class  EmployeeWage{
-	private static final  int IS_PART_TIME=1;
-	private static final int IS_FULL_TIME=2;
-   private static final int  TOTAL_COMPANIES =20;
-	private static int EMP_RATE_PER_HOUR = 0;
-	private static int NUM_OF_WORKING_DAY = 0;
-	private static int MAX_HRS_IN_MONTH = 0;
-	int totalEmpWage;
+//USING INTERFACE CLASS//
+interface MultipleCompanies	{
 
- 	public static void main (String args[]){
-		EmployeeWage2 employeeWage[] = new EmployeeWage2[TOTAL_COMPANIES];
-		for (int index = 0; index < TOTAL_COMPANIES; index++) {
-			//Generate Employee rate between 100 to 300
-			EMP_RATE_PER_HOUR = (int) (100 + Math.random() * 200);
-
-			//Generate Employee working days between 20 to 25
-			NUM_OF_WORKING_DAY = (int) (20 + Math.random() * 5);
-
-			//Generate Employee work hours between 100 to 200
-			MAX_HRS_IN_MONTH = (int) (100 + Math.random() * 100);
-			employeeWage[index] = new EmployeeWage2(EMP_RATE_PER_HOUR,NUM_OF_WORKING_DAY,MAX_HRS_IN_MONTH);
-			employeeCompany( employeeWage[index], index);
-		}
-	}
-
-	public static void employeeCompany(EmployeeWage2 employeeWage, int empNo){ 
-		//WE ARE TAKING LOCAL VARIABLE
-		int empHrs=0,totalEmpHrs=0,totalWorkingDays=0,empWage=0,totalEmpWage=0;
-		while (totalEmpHrs <= employeeWage.getMAX_HRS_IN_MONTH() && totalWorkingDays < employeeWage.getNUM_OF_WORKING_DAY()){
-				totalWorkingDays++;
-				int empCheck = (int) (Math.random()*10)%3;
-				switch (empCheck){
-					case IS_PART_TIME:
-							empHrs=4;
-							break;
-					case IS_FULL_TIME:
-							empHrs=8;
-							break;
-					default:
-							empHrs=0;
-				}
-				totalEmpHrs += empHrs;
-				empWage = empHrs * employeeWage.getEMP_RATE_PER_HOUR();
-				totalEmpWage += empWage;
-		}
-		employeeWage.setEmpWage(totalEmpWage);
-		System.out.println("Monthly wages of employee:"+ (empNo + 1)+ ": " +totalEmpWage);
-	}
-
+	public void totalEmpWageMath( EmpWageBuilder obj );
 }
-class EmployeeWage2{
-	private final int EMP_RATE_PER_HOUR;
-	private final int NUM_OF_WORKING_DAY;
-	private final int MAX_HRS_IN_MONTH;
-	private  int totalEmpWage=0;
 
-	//WE ARE TAKING CONSTRUCTOR PARAMETER
-	public EmployeeWage2(int EMP_RATE_PER_HOUR,int NUM_OF_WORKING_DAY,int MAX_HRS_IN_MONTH){
-		this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
-		this.NUM_OF_WORKING_DAY = NUM_OF_WORKING_DAY;
-		this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
+public class  EmployeeWage implements MultipleCompanies{
+	public static final  int IS_PART_TIME=1;
+	public static final int IS_FULL_TIME=2;
+	public static int EMP_RATE_PER_HOUR;
+	public static int NUM_OF_WORKING_DAYS;
+	public static int MAX_HRS_IN_MONTH;
+
+	public void totalEmpWageMath( EmpWageBuilder company ){
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		while( totalEmpHrs <= company.getMaxHrsInMonth() && totalWorkingDays < company.getMaxNumOfWorkingDays() )  {
+			totalWorkingDays++;
+			int empCheck = (int) ( Math.random() * 10 ) % 3;
+			switch (empCheck) {
+				case IS_PART_TIME:
+						empHrs = 4;
+						break;
+				case IS_FULL_TIME:
+						empHrs = 8;
+						break;
+				default:
+						empHrs = 0;
+			}
+			totalEmpHrs += empHrs;
+			System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +  empHrs);
+		}
+		company.setTotalEmpWage( totalEmpHrs * company.getEmpRatePerHour() );
+		System.out.println( "Employee is " + company.getTotalEmpWage());	
 	}
-	public int getEMP_RATE_PER_HOUR(){
+
+	public static void main(String[] args){
+		EmployeeWage empwage = new EmployeeWage();
+		//DECLARE ARRAY
+		EmpWageBuilder[] company = new EmpWageBuilder[2];
+
+		company[0] = new EmpWageBuilder( 20,25,80 );
+		empwage.totalEmpWageMath( company[0] );
+		company[1] = new EmpWageBuilder( 30,20,100 );
+   	empwage.totalEmpWageMath( company[1] );
+	}
+}
+
+class EmpWageBuilder{
+
+	public static int EMP_RATE_PER_HOUR;
+	public static int NUM_OF_WORKING_DAYS;
+	public static int MAX_HRS_IN_MONTH;	
+	int totalEmpWage;	
+	
+	public EmpWageBuilder( int EMP_RATE_PER_HOUR,int NUM_OF_WORKING_DAYS,int MAX_HRS_IN_MONTH ){
+			this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
+			this.NUM_OF_WORKING_DAYS = NUM_OF_WORKING_DAYS;
+			this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
+	}
+	//USING SETTER AND GETTER METHOD
+	public int getEmpRatePerHour()	{
 		return EMP_RATE_PER_HOUR;
 	}
-	public int getNUM_OF_WORKING_DAY(){
-		return NUM_OF_WORKING_DAY;
+
+	public int getMaxNumOfWorkingDays()	{
+		return NUM_OF_WORKING_DAYS;
 	}
-	public int getMAX_HRS_IN_MONTH(){
+
+	public int getMaxHrsInMonth()	{
 		return MAX_HRS_IN_MONTH;
 	}
-	public void setEmpWage(int totalEmpWage){
+
+	public void setTotalEmpWage( int totalEmpWage )	{
 		this.totalEmpWage = totalEmpWage;
 	}
-}
 
+	public int getTotalEmpWage()  {
+	return totalEmpWage;
+	}
+	System.out.println( +totalEmpWage )
+}
